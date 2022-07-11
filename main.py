b407864deda5868a5430366272238b63b2d8f58a
romankravchuk <kravchuk.roman02@gmail.com>
2022-07-11 22:45:27 +0800

    refactor main script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 
-from rich.console import Console
+from datetime import datetime
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -12,66 +12,47 @@ from googleapiclient.http import MediaFileUpload
 
 import config
 
-from logger import logger
+from utils import console, logger
 
 
-def create_image_folder(folder, folder_id, service, console) -> str:
-    img_metadata = {
-        'name': config.IMG_FOLDER,
-        'mimeType': config.MIME_TYPE,
-        'parents': [folder_id]
-    }
-    img_folder = service.files() \
-                        .create(body=img_metadata, fields='id') \
-                        .execute()
-    img_folder_id = img_folder.get('id')
-
-    console.print(
-        f' [•] Created an image folder for {folder}.',
-        style='bold green'
-    )
-
-    return img_folder_id
+def create_file(service, body: dict, media_body: MediaFileUpload):
+    service.files().create(
+        body=body,
+        media_body=media_body,
+    ).execute()
 
 
-def create_files(
-        parent,
-        folder,
-        folder_id,
-        service,
-        console,
-        is_img_folder=False) -> None:
-    response = service.files().list(
-        q=f"parents='{folder_id}'",
-        fields='files(id, name)'
-    ).execute()['files']
+def create_folder(service, body):
+    folder = service.files().create(
+        body=body,
+        fields='id'
+    ).execute()
+    return folder
 
-    folder_path = os.path.abspath(f'{parent}/{folder}')
-    folder_files = os.listdir(folder_path)
-    existing_files: list[str] = [
-        item['name'] for item in response if item['name'] in folder_files
-    ]
 
-    if not is_img_folder and config.IMG_FOLDER not in existing_files:
-        img_folder_id = create_image_folder(
-            folder=folder,
-            folder_id=folder_id,
-            service=service,
-            console=console
-        )
-        create_files(
-            parent=f'{parent}/{folder}',
-            folder=config.IMG_FOLDER,
-            folder_id=img_folder_id,
-            service=service,
-            console=console,
-            is_img_folder=True
-        )
+def create_files(service, folder: str, folder_id):
+    folder_path = os.path.abspath(folder)
 
     for file in os.listdir(folder_path):
-        file_path = f'{folder_path}/{file}'
+        file_path = os.path.abspath(f'{folder}/{file}')
+        
+        if os.path.isdir(file_path):
+            if file.startswith('.'):
+                console.print(f' [•] Can not backup {file}. It\'s a secret folder.', style='red bold')
+            folder_metadata = {
+                'name': file,
+                'mimeType': config.MIME_TYPE,
+                'parents': [folder_id]
+            }
 
-        if os.path.isdir(os.path.abspath(file_path)) or file in existing_files:
+            child_folder : dict = create_folder(service, folder_metadata)
+            console.print(f' [•] Create the folder {file} for {folder}', style='green bold')
+
+            create_files(
+                service=service,
+                folder=f"{folder}/{file}",
+                folder_id=child_folder.get('id')
+            )
             continue
 
         file_metadata = {
@@ -79,70 +60,15 @@ def create_files(
             'parents': [folder_id]
         }
 
-        media = MediaFileUpload(file_path)
+        media_file_upload = MediaFileUpload(file_path)
 
-        service.files().create(
-            body=file_metadata,
-            media_body=media,
-            fields='id'
-        ).execute()
-
-        console.print(f' [•] Backed up a file: {file}', style='bold green')
-
-
-def create_folders(parent, folder_id, service, console: Console) -> None:
-    parent_folder = service.files().list(
-        q=f'parents="{folder_id}"',
-        spaces='drive',
-        fields='files(id, name)'
-    ).execute()
-
-    folders = [folder for folder in parent_folder['files']]
-
-    for folder in os.listdir(os.path.abspath(parent)):
-        if os.path.isfile(folder):
-            console.print(f' [•] Can not create {folder}. It is a file.', style='red bold')
+        try:
+            create_file(service, file_metadata, media_file_upload)
+        except HttpError:
+            console.print(f' [•] Can not backup {file}', style='red bold')
             continue
 
-        if folder == config.OBSIDIAN_FOLDER or folder.startswith('.'):
-            console.print(f' [•] Can not create {folder}. It is a secret folder.', style='red bold')
-            continue
-
-        if folder in [item['name'] for item in folders]:
-            console.print(f' [•] Folder {folder} already exists in the drive.', style='red bold')
-            continue
-
-        file_metadata = {
-            'name': folder,
-            'mimeType': config.MIME_TYPE,
-            'parents': [folder_id]
-        }
-        drive_folder = None
-        for item in folders:
-            if item['name'] == folder:
-                drive_folder = service.files().list(
-                    q=f'name="{folder}"',
-                    spaces='drive',
-                    fields='files(id, name)'
-                ).execute()
-                drive_folder_id = drive_folder['files'][0]['id']
-                break
-
-        if drive_folder is None:
-            drive_folder = service.files().create(body=file_metadata).execute()
-            drive_folder_id = drive_folder.get('id')
-            console.print(
-                f' [•] Created a folder: {drive_folder["name"]}',
-                style='bold green'
-            )
-
-        create_files(
-            parent=parent,
-            folder=folder,
-            folder_id=drive_folder_id,
-            service=service,
-            console=console
-        )
+        console.print(f' [•] Backed up a file: {file}', style='green bold')
 
 
 def try_get_from_token_file() -> tuple[Credentials | None, str]:
@@ -159,55 +85,83 @@ def try_get_from_token_file() -> tuple[Credentials | None, str]:
     return credentials, message
 
 
-def main() -> None:
-    console = Console()
-
+def get_credentials() -> Credentials:
     console.print(' [•] Checking for a token file...', style='green bold')
-    creds, message = try_get_from_token_file()
+    credentials, message = try_get_from_token_file()
     console.print(message, style='green bold')
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 config.CREDENTIALS_FILE, config.SCOPES)
-            creds = flow.run_local_server(port=0)
+            credentials = flow.run_local_server(port=0)
         console.print(' [•] Creating a token file...', style='green bold')
         with open(config.TOKEN_FILE, 'w') as token:
-            token.write(creds.to_json())
+            token.write(credentials.to_json())
         console.print(' [•] The token file created', style='green bold')
+    return credentials
 
+
+def build_service(credentials):
+    return build('drive', 'v3', credentials=credentials)
+
+
+def backup_files() -> None:
     try:
-        service = build('drive', 'v3', credentials=creds)
-
-        response = service.files().list(
-            q=f'name="{config.BACKUP_FOLDER}" and mimeType="{config.MIME_TYPE}"',
-            spaces='drive'
+        credentials = get_credentials()
+    except Exception:
+        console.print(' [•] Something went wrong...', style='red bold')
+        return
+    
+    try:
+        service = build_service(credentials=credentials)
+        folder_metadata = {
+            'name': f'{config.BACKUP_FOLDER}-{datetime.now().strftime("%m.%d.%y-%H:%M:%S")}',
+            'mimeType': config.MIME_TYPE
+        }
+        folder = service.files().create(
+            body=folder_metadata,
+            fields='id'
         ).execute()
-
-        if not response['files']:
-            file_metadata = {
-                'name': config.BACKUP_FOLDER,
-                'mimeType': config.MIME_TYPE
-            }
-
-            folder = service.files() \
-                            .create(body=file_metadata, fields='id') \
-                            .execute()
-
-            folder_id = folder.get('id')
-        else:
-            folder_id = response['files'][0]['id']
-
-        try:
-            console.print(' [•] Start creating folders...', style='green bold')
-            create_folders(config.BACKUP_FOLDER, folder_id, service, console)
-            console.print(' [•] All done.', style='blue bold')
-        except FileNotFoundError as e:
-            logger.error(e)
+        folder_id = folder.get('id')
+        
+        console.print(' [•] Start creating folders...', style='green bold')
+        create_files(
+            service=service,
+            folder=config.BACKUP_FOLDER,
+            folder_id=folder_id
+        )
+        console.print(' [•] All done.', style='blue bold')
     except HttpError as e:
-        logger.error(e.content.decode())
+        logger.error(e)
+
+
+def choose_command():
+    console.print(' [•] B - Backup\n [•] D - Download\n [•] E - Exit', style='blue bold')
+    while True:
+        command = console.input(' [•] Enter the command: ')
+        if command in ['B', 'D', 'E']:
+            return command
+        console.print(' [•] Unknown command... Try again.', style='yellow bold')
+        
+
+def main():
+    try:
+        command = choose_command()
+        match command:
+            case 'B':
+                backup_files()
+            case 'D':
+                console.print(' [•] Comming soon...', style='yellow bold')
+                # download_files()
+                return
+            case 'E':
+                console.print(' [•] Exit from program...', style='blue bold')
+                return
+    except KeyboardInterrupt | Exception:
+        return
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
+from rich.console import Console
 from loguru import logger
+
 
 logger.add(
     sink='./logs/error.log',
     level='ERROR',
     format='{time} | {level} | {message}'
 )
+
+console = Console()
